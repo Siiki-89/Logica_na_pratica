7cac06c6a080006840e0d23c607d3d780aed05f9
Alexandre de Melo <84542748+Siiki-89@users.noreply.github.com>
2025-03-19 17:26:03 -0300

    Ex1: Divisão do maior p/ menor

---
+++ LogicaNaPratica/src/logicanapratica/ex1.java
@@ -0,0 +1,34 @@
+
+package logicanapratica;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        //Declaração das variáveis
+        double maior = 0, menor = 0, resultado;
+        //Solicita os números ao usuario
+        System.out.print("Digite o primeiro número decimal: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número decimal: ");
+        double num2 = scanner.nextDouble();
+        //Verifica qual numero é maior
+        if (num1 > num2) {
+            maior = num1;
+            menor = num2;
+        } else {
+            maior = num2;
+            menor = num1;
+        }
+        //Calcula a divisão
+        resultado = maior / menor;
+        //Exibe o resultado
+        System.out.println("O resultado da divisão do maior pelo menor é: " + resultado);
+    }
+    
+}
